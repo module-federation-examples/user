65c7ee80e4e49a610cb3cc7295846d9deacad061
Sercan Eraslan <info@sercaneraslan.com>
2021-10-29 17:51:57 +0300

    create user app

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const deps = require('./package.json').dependencies;
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    port: 3003,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-env',
+            '@babel/preset-react',
+          ],
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin(
+      {
+        name: 'USER',
+        filename: 'remoteEntry.js',
+        shared: [
+          {
+            ...deps,
+            react: { requiredVersion: deps.react, singleton: true },
+            'react-dom': {
+              requiredVersion: deps['react-dom'],
+              singleton: true,
+            },
+          },
+        ],
+      }
+    ),
+    new HtmlWebpackPlugin({
+      template:
+        './public/index.html',
+    }),
+  ],
+};
